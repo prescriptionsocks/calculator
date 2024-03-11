99d80c6b2d0757d30ca12a5637ed240ac9557eda
Baraka <barakaarunga@gmail.com>
2024-03-11 14:57:58 +0300

    added operate function that call relevant math function based on the selected operator

--- script.js
+++ script.js
@@ -3,34 +3,47 @@ const add = function (firstNumber, secondNumber){
     return firstNumber + secondNumber
 }
 
-const addResult = add(2,3,4)
-console.log(addResult)
+
 
 //subtract
 const subtract = function(firstNumber, secondNumber){
     return firstNumber - secondNumber
 }
 
-const subResult = subtract(3,1,0)
-console.log(subResult)
+
 
 //multiply
 const multiply = function(firstNumber, secondNumber){
     return firstNumber * secondNumber
 }
 
-const mulResult = multiply(2,3,2)
-console.log(mulResult)
+
 
 //devide
 const devide = function(firstNumber,secondNumber){
     return firstNumber/secondNumber
 }
-const divResult = devide(12,3,2)
-console.log(divResult)
+
 
 //create variable for numbers & operators
 const firstNumber = 3
 const secondNumber = 4
-const operator = "+"
+const operator = '-'
+
+const operate = function(firstNumber, secondNumber, operator){
+    if (operator === '+'){
+        return add(firstNumber,secondNumber)
+    } else if (operator === '-') {
+        return subtract(firstNumber,secondNumber)
+    } else if (operator === '*') {
+        return multiply(firstNumber,secondNumber)
+    } else if (operator === '/') {
+        return devide(firstNumber,secondNumber)
+    }
+
+}
+
+const mazematics = operate(firstNumber, secondNumber, operator)
+console.log(mazematics)
+
 
