dde3cb497aed4b3516730c2bf805c1d36ebfcf87
Baraka <barakaarunga@gmail.com>
2024-03-11 14:42:54 +0300

    replaced array method math with basic operators math. It makes it easier and more direct to work with variables(avoids arrays)

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
 //create functions for basic math
-const add = function (...numbers){
-    return numbers.reduce((n,number)=>{
-        return n+number
-    }, 0)
+const add = function (firstNumber, secondNumber){
+    return firstNumber + secondNumber
 }
 
 const addResult = add(2,3,4)
 console.log(addResult)
 
 //subtract
-const subtract = function (...numbers){
-    return numbers.reduce((n,number)=>{
-        return n - number
-    })
+const subtract = function(firstNumber, secondNumber){
+    return firstNumber - secondNumber
 }
 
 const subResult = subtract(3,1,0)
 console.log(subResult)
 
 //multiply
-const multiply = function(...numbers){
-    return numbers.reduce((n,number)=>{
-        return n * number
-    })
+const multiply = function(firstNumber, secondNumber){
+    return firstNumber * secondNumber
 }
 
 const mulResult = multiply(2,3,2)
 console.log(mulResult)
 
 //devide
-const devide = function(...numbers){
-    return numbers.reduce((n,number)=>{
-        return n/number
-    })
+const devide = function(firstNumber,secondNumber){
+    return firstNumber/secondNumber
 }
-
 const divResult = devide(12,3,2)
 console.log(divResult)
+
 //create variable for numbers & operators
-//
+const firstNumber = 3
+const secondNumber = 4
+const operator = "+"
+
